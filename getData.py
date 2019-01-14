5fc29513915e8301951b2d32b7a56246a2ff0880
Konrad Semmler <semmlerkonrad@gmail.com>
2019-01-14 21:38:33 +0100

    Fixed API calls.

--- getData.py
+++ getData.py
@@ -2,18 +2,15 @@ import json
 import requests
 import base64
 import urllib
-from flask import Flask, request, redirect, g, render_template
 
 # Client Keys
-CLIENT_ID = "064745a28ab54fac909a96e10d08b56e"
-CLIENT_SECRET = "73a84c033c8448d1aac2cda5b2dfc42e"
+CLIENT_ID = input()
+CLIENT_SECRET = input()
 
 #Spotify URLS
 SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+SPOTIFY_API_URL = "https://api.spotify.com/v1"
 
 #Server-side Parameters
 CLIENT_SIDE_URL = "http://127.0.0.1"
@@ -24,36 +21,35 @@ STATE = ""
 SHOW_DIALOG_bool = True
 SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
-#Authorization of application with spotify
+#Authorization of application with Spotify
 def app_Authorization():
     auth_query_parameters = {
         "response_type": "code",
         "redirect_uri": REDIRECT_URI,
         "scope": SCOPE,
-        # "state": STATE,
-        # "show_dialog": SHOW_DIALOG_str,
+        "state": STATE,
+        "show_dialog": SHOW_DIALOG_str,
         "client_id": CLIENT_ID
     }
     url_args = "&".join(["{}={}".format(key,urllib.parse.quote(val)) for key,val in auth_query_parameters.items()])
     auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
     return auth_url
 
-#User allows us to acces there spotify
+#User allows us to access there spotify
 def user_Authorization():
-    auth_token = request.args['code']
-    code_payload = {
-        "grant_type": "authorization_code",
-        "code": str(auth_token),
-        "redirect_uri": REDIRECT_URI
-    }
-    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
-    headers = {"Authorization": "Basic {}".format(base64encoded)}
+    #auth_token = request.args['code']
+    code_payload = {"grant_type": "client_credentials"}
+    string_bytes = "{}:{}".format(CLIENT_ID, CLIENT_SECRET).encode("ascii")
+    base64encoded = base64.b64encode(string_bytes)
+    headers = {"Authorization": "Basic {}".format(base64encoded.decode('ascii'))}
     post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
 
     # Tokens are Returned to Application
     response_data = json.loads(post_request.text)
+    with open('response.json', 'w') as outfile:
+        json.dump(response_data, outfile)
+
     access_token = response_data["access_token"]
-    refresh_token = response_data["refresh_token"]
     token_type = response_data["token_type"]
     expires_in = response_data["expires_in"]
 
@@ -84,5 +80,15 @@ def Album_Data(header,profile,limit,offset):
     artist_response = requests.get(artist_api_endpoint, headers=header)
     artist_data = json.loads(artist_response.text)
     return artist_data
+
+def getAudioAnalysis(trackId, authToken):
+    chosenTrackEndpoint = "{0}/audio-analysis/{1}".format(SPOTIFY_API_URL, trackId)
+    #authHeader = {"Authorization": "Bearer {}".format(authToken)}
+
+    getRequest  = requests.get(chosenTrackEndpoint, headers = authToken)
+    analysisData = json.loads(getRequest.text)
+    with open('DANGERZONE.json', 'w') as outfile:
+        json.dump(analysisData, outfile)
+    return analysisData
 
 
