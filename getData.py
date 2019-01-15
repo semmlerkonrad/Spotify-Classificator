a11f0e1935b37805dc3dccf69cd5589de69a1dcc
Konrad Semmler <semmlerkonrad@gmail.com>
2019-01-15 23:40:17 +0100

    Tracks now download with all required parameters

--- getData.py
+++ getData.py
@@ -1,7 +1,6 @@
 import json
 import requests
 import base64
-import pandas as pd
 
 #Wywaliłem hardcoded klucze, bo technicznie nie powinny być widoczne publicznie xd. Najłatwiej to samemu zamienić u siebie w edytorze
 CLIENT_ID = input()
@@ -32,7 +31,6 @@ def getAuthToken():
     authHeader = {"Authorization":"Bearer {}".format(accessToken)}
     return authHeader
 
-
 def getAudioAnalysis(trackID, authToken):
     #adres GET do wykonania tego zapytania, wystarczy podmienić ten adres na jakikolwiek inny podany w Docs i powinno działać
     chosenTrackEndpoint = "{0}/audio-analysis/{1}".format(apiURL, trackID)
@@ -40,42 +38,67 @@ def getAudioAnalysis(trackID, authToken):
 
     # dane z low-level analizy Audio zwrócone przez serwer
     analysisData = json.loads(getRequest.text)
-
-
-    #zapisujemy do pliku json na wszelki wypadek i potem zwracamy do funkcji, żeby przetworzyć
-    #with open('DANGERZONE.json', 'w') as outfile:
-    #    json.dump(analysisData, outfile)
     return analysisData
 
+def getAudioFeatures(trackID, authToken):
+    chosenTrackEndpoint = "{0}/audio-features/{1}".format(apiURL, trackID)
+    getRequest  = requests.get(chosenTrackEndpoint, headers = authToken)
+    analysisData = json.loads(getRequest.text)
+    return analysisData
 
-def getTrackAndAlbumIDsFromPlaylist(playlistID, authToken):
-    #declare columns and content of the DataFrame
-    cols=['trackID','albumID', 'track Genre']
+def getTrackAndAlbumIDsFromPlaylist(playlistID, authToken, genre):
     listOfID=[]
 
     chosenPlaylistEndpoint = "{0}/playlists/{1}/tracks".format(apiURL, playlistID)
     getRequest = requests.get(chosenPlaylistEndpoint, headers=authToken)
 
     playlistItems = json.loads(getRequest.text)
-    for item in playlistItems['items']:
-        trackID = item['track']['id']
-        albumID = item['track']['album']['id']
-        albumGenre = getAlbumGenres(albumID,authToken)
-        listOfID.append([trackID,albumID, albumGenre])
-    dataAll = pd.DataFrame(listOfID, columns=cols)
-    # dataAll.to_csv('newcs.csv')
+    # with open('error.json', 'w') as outfile:
+    #     json.dump(playlistItems, outfile)
+    if getRequest.ok:
+        for item in playlistItems['items']:
+            trackID = item['track']['id']
+            albumID = item['track']['album']['id']
 
-    return dataAll
+            trackAudioFeatures = getAudioFeatures(trackID,authToken)
+            trackAudioData = getAudioAnalysis(trackID,authToken)
+
+            duration = trackAudioData['track']['duration']
+            endOfFadeIn = trackAudioData['track']['end_of_fade_in']
+            startOfFadeOut = trackAudioData['track']['start_of_fade_out']
+            loudness = trackAudioData['track']['loudness']
+            tempo = trackAudioData['track']['tempo']
+            timeSignature = trackAudioData['track']['time_signature']
+            mode = trackAudioData['track']['mode']
+            key = trackAudioFeatures['key']
+            acousticness = trackAudioFeatures['acousticness']
+            danceability = trackAudioFeatures['danceability']
+            energy = trackAudioFeatures['energy']
+            instrumentalness = trackAudioFeatures['instrumentalness']
+            liveness = trackAudioFeatures['liveness']
+            speechiness = trackAudioFeatures['speechiness']
+            valence = trackAudioFeatures['valence']
+            numberOfSections = len(trackAudioData['sections'])
+
+            listOfID.append([trackID,albumID, genre, duration, endOfFadeIn, startOfFadeOut, loudness, tempo,
+                             timeSignature, mode, key, acousticness, danceability, energy, instrumentalness, liveness,
+                             speechiness, valence, numberOfSections])
+
+    return listOfID
 
 def getAlbumGenres(albumID, authToken):
     chosenAlbumEndpoint = "{0}/albums/{1}".format(apiURL,albumID)
     getRequest = requests.get(chosenAlbumEndpoint, headers=authToken)
 
     albumItems = json.loads(getRequest.text)
-    firstGenre = albumItems['genres'][0]
+    with open('album.json', 'w') as outfile:
+        json.dump(albumItems, outfile)
+    firstGenre = albumItems['genres']
+    if len(firstGenre)>0:
+        firstGenre = firstGenre[0]
+    else:
+        firstGenre = 'None'
     return firstGenre
 
-
-
-
 #TODO wykrywanie kodów odpowiedzi Serwera i reagowanie na błędy
+#TODO zamień flask na urllib
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, json, redirect
+from flask import Flask, redirect
 import getData
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,15 +10,31 @@ def index():
 
 @app.route("/callback/q")
 def callback():
-
     authToken = getData.getAuthToken()
 
-    #analiza audio DANGER ZONE
-    DANGERZONE = getData.getAudioAnalysis('34x6hEJgGAOQvmlMql5Ige', authToken)
+    listAllData=[]
+    getData.getTrackAndAlbumIDsFromPlaylist('37i9dQZF1DX2vTOtsQ5Isl', authToken, 'pop')
+    playlistIDs = {
+        #'37i9dQZF1DX2vTOtsQ5Isl':'Pop',
+        #'37i9dQZF1DX0XUsuxWHRQd':'Rap',
+        # '37i9dQZF1DWXRqgorJj26U':'Rock',
+        #'37i9dQZF1DX3xuESxW8V9F':'EDM',
+                   # '37i9dQZF1DXa8NOEUWPn9W':'House',
+                   # '37i9dQZF1DX6J5NfMJS675':'Techno',
+                   # '37i9dQZF1DXbXD9pMSZomS':'Lo-fi House',
+                   # '37i9dQZF1DXbITWG1ZJKYt':'Jazz'
+    }
 
-    myPlaylistTracks = getData.getTrackAndAlbumIDsFromPlaylist('5N13fntbZVqW2euiBTfbD2', authToken)
-    #zwracamy plik json to przeglądarki
-    return myPlaylistTracks.head()
+    for playlist, genre in playlistIDs.items():
+        currentTracks = getData.getTrackAndAlbumIDsFromPlaylist(playlist, authToken, genre)
+        listAllData+=currentTracks
+
+    cols = ['trackID', 'albumID', 'track Genre', 'duration', 'end Of Fade-In', 'start Of Fade-Out', 'loudness', 'tempo',
+            'time Signature', 'mode', 'key', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness',
+            'speechiness', 'valence', 'number Of Sections']
+    dataAll = pd.DataFrame(listAllData, columns=cols)
+    dataAll.to_csv('newcsv3.csv')
+    return "done"
 
 
 if __name__ == "__main__":
